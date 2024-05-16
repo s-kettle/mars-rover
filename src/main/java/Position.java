51ac61244acda2591c9f68ae2f1f582c0f194379
Sam Kettle <sam.kettle3@icloud.com>
2024-05-16 16:52:02 +0100

    refactor: add single parameter constructor to Position class

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -3,6 +3,12 @@ public class Position {
     private int y;
     private Direction facing;
 
+    public Position(Direction facing) {
+        this.x = 0;
+        this.y = 0;
+        this.facing = facing;
+    }
+
     public Position(int x, int y, Direction facing) {
         this.x = x;
         this.y = y;
@@ -19,5 +25,17 @@ public class Position {
 
     public Direction getFacing() {
         return facing;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setFacing(Direction facing) {
+        this.facing = facing;
     }
 }
