15f99dff90a9a8290ddd803c7503222ccb0c5aa8
Sam Kettle <sam.kettle3@icloud.com>
2024-05-16 16:52:39 +0100

    feat: add fields to Plateau class

--- src/main/java/Plateau.java
+++ src/main/java/Plateau.java
@@ -1,2 +1,12 @@
 public class Plateau {
+
+    private final PlateauSize size;
+
+    public Plateau(PlateauSize size) {
+        this.size = size;
+    }
+
+    public PlateauSize getSize() {
+        return size;
+    }
 }
