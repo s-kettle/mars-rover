dc869fed93353ba0815f50783326b847efaeeb5e
Sam Kettle <sam.kettle3@icloud.com>
2024-05-16 14:21:11 +0100

    feat: Rover move() functionality added with test

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -1,2 +1,39 @@
+import java.util.List;
+
 public class Rover {
+
+    private Position position;
+
+    public Rover(Position position) {
+        this.position = position;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void move(){
+
+        Direction currentFacing = position.getFacing();
+
+        switch (currentFacing) {
+            case N -> {
+                this.position.setY(position.getY() + 1);
+            }
+            case E -> {
+                this.position.setX(position.getX() + 1);
+            }
+            case S -> {
+                this.position.setY(position.getY() - 1);
+            }
+            case W -> {
+                this.position.setX(position.getX() - 1);
+            }
+        }
+    }
+
+//    public Position rotate() {
+//
+//    }
+
 }
--- src/test/java/RoverTest.java
+++ src/test/java/RoverTest.java
@@ -1,4 +1,36 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class RoverTest {
-  
+
+    @Test
+    @DisplayName("Rover position updates on move()")
+    void moveTest() {
+
+        Position positionNorth = new Position(2, 2, Direction.N);
+        Position positionEast = new Position(2, 2, Direction.E);
+        Position positionSouth = new Position(2, 2, Direction.S);
+        Position positionWest = new Position(2, 2, Direction.W);
+
+        Rover roverNorth = new Rover(positionNorth);
+        Rover roverEast = new Rover(positionEast);
+        Rover roverSouth = new Rover(positionSouth);
+        Rover roverWest = new Rover(positionWest);
+
+        roverNorth.move();
+        roverSouth.move();
+        roverEast.move();
+        roverWest.move();
+
+        assertEquals(3, roverNorth.getPosition().getY());
+        assertEquals(3, roverEast.getPosition().getX());
+        assertEquals(1, roverSouth.getPosition().getY());
+        assertEquals(1, roverWest.getPosition().getX());
+    }
+
+    @Test
+    void rotate() {
+    }
 }
